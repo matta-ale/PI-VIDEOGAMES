2d1f2d9cd249ce829aac9d92777373393a0b0752
matta-ale <matta.ale@gmail.com>
2023-12-17 19:13:39 -0300

    ajustes menores para deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,8 +6,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {store} from './redux/store';
 import axios from 'axios'
+import config from '../config';
 
-axios.defaults.baseURL= 'http://localhost:3001'
+axios.defaults.baseURL= config.apiUrl
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
